b8139833c962085dfc1281e17ad7a6a3067a21e8
杨卫东 <davidyang_cool@icloud.com>
2018-05-08 15:48:11 +0800

    Learn the new course about MySQL

---
+++ mysql_crashcourse04.sql
@@ -0,0 +1,318 @@
+Last login: Mon May  7 17:49:05 on console
+david:~ david$ mysql -u root -p
+Enter password: 
+Welcome to the MySQL monitor.  Commands end with ; or \g.
+Your MySQL connection id is 2
+Server version: 5.7.21 MySQL Community Server (GPL)
+
+Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
+
+Oracle is a registered trademark of Oracle Corporation and/or its
+affiliates. Other names may be trademarks of their respective
+owners.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| crashcourse        |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+5 rows in set (0.01 sec)
+
+mysql> USE crashcourse;
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+Database changed
+mysql> SHOW TABLES;
++-----------------------+
+| Tables_in_crashcourse |
++-----------------------+
+| customers             |
+| orderitems            |
+| orders                |
+| productnotes          |
+| products              |
+| vendors               |
++-----------------------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT * FROM products;
++---------+---------+----------------+------------+----------------------------------------------------------------+
+| prod_id | vend_id | prod_name      | prod_price | prod_desc                                                      |
++---------+---------+----------------+------------+----------------------------------------------------------------+
+| ANV01   |    1001 | .5 ton anvil   |       5.99 | .5 ton anvil, black, complete with handy hook                  |
+| ANV02   |    1001 | 1 ton anvil    |       9.99 | 1 ton anvil, black, complete with handy hook and carrying case |
+| ANV03   |    1001 | 2 ton anvil    |      14.99 | 2 ton anvil, black, complete with handy hook and carrying case |
+| DTNTR   |    1003 | Detonator      |      13.00 | Detonator (plunger powered), fuses not included                |
+| FB      |    1003 | Bird seed      |      10.00 | Large bag (suitable for road runners)                          |
+| FC      |    1003 | Carrots        |       2.50 | Carrots (rabbit hunting season only)                           |
+| FU1     |    1002 | Fuses          |       3.42 | 1 dozen, extra long                                            |
+| JP1000  |    1005 | JetPack 1000   |      35.00 | JetPack 1000, intended for single use                          |
+| JP2000  |    1005 | JetPack 2000   |      55.00 | JetPack 2000, multi-use                                        |
+| OL1     |    1002 | Oil can        |       8.99 | Oil can, red                                                   |
+| SAFE    |    1003 | Safe           |      50.00 | Safe with combination lock                                     |
+| SLING   |    1003 | Sling          |       4.49 | Sling, one size fits all                                       |
+| TNT1    |    1003 | TNT (1 stick)  |       2.50 | TNT, red, single stick                                         |
+| TNT2    |    1003 | TNT (5 sticks) |      10.00 | TNT, red, pack of 10 sticks                                    |
++---------+---------+----------------+------------+----------------------------------------------------------------+
+14 rows in set (0.00 sec)
+
+mysql> SELECT prod_id,prod_price,prod_name
+    -> FROM products
+    -> WHERE vend_id = 1003 AND prod_price <=10;
++---------+------------+----------------+
+| prod_id | prod_price | prod_name      |
++---------+------------+----------------+
+| FB      |      10.00 | Bird seed      |
+| FC      |       2.50 | Carrots        |
+| SLING   |       4.49 | Sling          |
+| TNT1    |       2.50 | TNT (1 stick)  |
+| TNT2    |      10.00 | TNT (5 sticks) |
++---------+------------+----------------+
+5 rows in set (0.01 sec)
+
+mysql> SELECT prod_name,prod_price
+    -> FROM products
+    -> WHERE vend_id = 1002 OR vend_id = 1003;
++----------------+------------+
+| prod_name      | prod_price |
++----------------+------------+
+| Detonator      |      13.00 |
+| Bird seed      |      10.00 |
+| Carrots        |       2.50 |
+| Fuses          |       3.42 |
+| Oil can        |       8.99 |
+| Safe           |      50.00 |
+| Sling          |       4.49 |
+| TNT (1 stick)  |       2.50 |
+| TNT (5 sticks) |      10.00 |
++----------------+------------+
+9 rows in set (0.00 sec)
+
+mysql> SELECT prod_name,prod_price
+    -> FROM products
+    -> WHERE vend_id = 1002 OR vend_id =1003 AND  prod_price >= 10;
++----------------+------------+
+| prod_name      | prod_price |
++----------------+------------+
+| Detonator      |      13.00 |
+| Bird seed      |      10.00 |
+| Fuses          |       3.42 |
+| Oil can        |       8.99 |
+| Safe           |      50.00 |
+| TNT (5 sticks) |      10.00 |
++----------------+------------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT prod_name,prod_price FROM products WHERE vend_id = 1002 OR vend_id =1003 AND  prod_price >= 10
+    -> ;
++----------------+------------+
+| prod_name      | prod_price |
++----------------+------------+
+| Detonator      |      13.00 |
+| Bird seed      |      10.00 |
+| Fuses          |       3.42 |
+| Oil can        |       8.99 |
+| Safe           |      50.00 |
+| TNT (5 sticks) |      10.00 |
++----------------+------------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT prod_name,prod_price FROM products WHERE (vend_id = 1002 OR vend_id =1003) AND  prod_price >= 10;
++----------------+------------+
+| prod_name      | prod_price |
++----------------+------------+
+| Detonator      |      13.00 |
+| Bird seed      |      10.00 |
+| Safe           |      50.00 |
+| TNT (5 sticks) |      10.00 |
++----------------+------------+
+4 rows in set (0.00 sec)
+
+mysql> SELECT prod_name,prod_price
+    -> FROM products
+    -> WHERE vend_id IN (1002,1003)
+    -> ORDER BY prod_name;
++----------------+------------+
+| prod_name      | prod_price |
++----------------+------------+
+| Bird seed      |      10.00 |
+| Carrots        |       2.50 |
+| Detonator      |      13.00 |
+| Fuses          |       3.42 |
+| Oil can        |       8.99 |
+| Safe           |      50.00 |
+| Sling          |       4.49 |
+| TNT (1 stick)  |       2.50 |
+| TNT (5 sticks) |      10.00 |
++----------------+------------+
+9 rows in set (0.01 sec)
+
+mysql> SELECT prod_name,prod_price
+    -> FROM products
+    -> WHERE vend_id NOT IN (1002,1003)
+    -> ORDER BY prod_name;
++--------------+------------+
+| prod_name    | prod_price |
++--------------+------------+
+| .5 ton anvil |       5.99 |
+| 1 ton anvil  |       9.99 |
+| 2 ton anvil  |      14.99 |
+| JetPack 1000 |      35.00 |
+| JetPack 2000 |      55.00 |
++--------------+------------+
+5 rows in set (0.00 sec)
+
+mysql> CHAPTER 8;
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'CHAPTER 8' at line 1
+mysql> ;
+ERROR: 
+No query specified
+
+mysql> SELECT prod_id,prod_name
+    -> FROM  products
+    -> WHERE prod_name LIKE 'jet%';
++---------+--------------+
+| prod_id | prod_name    |
++---------+--------------+
+| JP1000  | JetPack 1000 |
+| JP2000  | JetPack 2000 |
++---------+--------------+
+2 rows in set (0.00 sec)
+
+mysql> SELECT prod_id,prod_name
+    -> FROM products
+    -> WHERE prod_name LIKE '%anvil%';
++---------+--------------+
+| prod_id | prod_name    |
++---------+--------------+
+| ANV01   | .5 ton anvil |
+| ANV02   | 1 ton anvil  |
+| ANV03   | 2 ton anvil  |
++---------+--------------+
+3 rows in set (0.00 sec)
+
+mysql> SELECT prod_id,prod_name
+    -> FROM products
+    -> WHERE prod_name LIKE '_ ton anvil';
++---------+-------------+
+| prod_id | prod_name   |
++---------+-------------+
+| ANV02   | 1 ton anvil |
+| ANV03   | 2 ton anvil |
++---------+-------------+
+2 rows in set (0.00 sec)
+
+mysql> SELECT prod_id,prod_name
+    -> FROM products
+    -> WHERE prod_name LIKE '% ton anvil';
++---------+--------------+
+| prod_id | prod_name    |
++---------+--------------+
+| ANV01   | .5 ton anvil |
+| ANV02   | 1 ton anvil  |
+| ANV03   | 2 ton anvil  |
++---------+--------------+
+3 rows in set (0.00 sec)
+
+mysql> SELECT *
+    -> FROM vendors;
++---------+----------------+-----------------+-------------+------------+----------+--------------+
+| vend_id | vend_name      | vend_address    | vend_city   | vend_state | vend_zip | vend_country |
++---------+----------------+-----------------+-------------+------------+----------+--------------+
+|    1001 | Anvils R Us    | 123 Main Street | Southfield  | MI         | 48075    | USA          |
+|    1002 | LT Supplies    | 500 Park Street | Anytown     | OH         | 44333    | USA          |
+|    1003 | ACME           | 555 High Street | Los Angeles | CA         | 90046    | USA          |
+|    1004 | Furball Inc.   | 1000 5th Avenue | New York    | NY         | 11111    | USA          |
+|    1005 | Jet Set        | 42 Galaxy Road  | London      | NULL       | N16 6PS  | England      |
+|    1006 | Jouets Et Ours | 1 Rue Amusement | Paris       | NULL       | 45678    | France       |
++---------+----------------+-----------------+-------------+------------+----------+--------------+
+6 rows in set (0.00 sec)
+
+mysql> 
+Display all 830 possibilities? (y or n) 
+mysql> SELECT Concat(vend_name,'(',vend_country,')')
+    -> FROM vendors
+    -> ORDER BY vend_name;
++----------------------------------------+
+| Concat(vend_name,'(',vend_country,')') |
++----------------------------------------+
+| ACME(USA)                              |
+| Anvils R Us(USA)                       |
+| Furball Inc.(USA)                      |
+| Jet Set(England)                       |
+| Jouets Et Ours(France)                 |
+| LT Supplies(USA)                       |
++----------------------------------------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT Concat(RTrim(vend_name),'(',RTrim(vend_country),')') AS vend_title
+    -> FROM vendors
+    -> ORDER BY vend_name;
++------------------------+
+| vend_title             |
++------------------------+
+| ACME(USA)              |
+| Anvils R Us(USA)       |
+| Furball Inc.(USA)      |
+| Jet Set(England)       |
+| Jouets Et Ours(France) |
+| LT Supplies(USA)       |
++------------------------+
+6 rows in set (0.00 sec)
+
+mysql> SELECT * FROM orderitems;
++-----------+------------+---------+----------+------------+
+| order_num | order_item | prod_id | quantity | item_price |
++-----------+------------+---------+----------+------------+
+|     20005 |          1 | ANV01   |       10 |       5.99 |
+|     20005 |          2 | ANV02   |        3 |       9.99 |
+|     20005 |          3 | TNT2    |        5 |      10.00 |
+|     20005 |          4 | FB      |        1 |      10.00 |
+|     20006 |          1 | JP2000  |        1 |      55.00 |
+|     20007 |          1 | TNT2    |      100 |      10.00 |
+|     20008 |          1 | FC      |       50 |       2.50 |
+|     20009 |          1 | FB      |        1 |      10.00 |
+|     20009 |          2 | OL1     |        1 |       8.99 |
+|     20009 |          3 | SLING   |        1 |       4.49 |
+|     20009 |          4 | ANV03   |        1 |      14.99 |
++-----------+------------+---------+----------+------------+
+11 rows in set (0.00 sec)
+
+mysql> SELECT prod_id,quantity,item_price
+    -> FROM orderitems
+    -> WHERE order_num = 20005;
++---------+----------+------------+
+| prod_id | quantity | item_price |
++---------+----------+------------+
+| ANV01   |       10 |       5.99 |
+| ANV02   |        3 |       9.99 |
+| TNT2    |        5 |      10.00 |
+| FB      |        1 |      10.00 |
++---------+----------+------------+
+4 rows in set (0.00 sec)
+
+mysql> SELECT  prod_id,
+    -> quantity,
+    -> item_price,
+    -> quantity*item_price AS expanded_price
+    -> FROM orderitems
+    -> WHERE order_num = 20005;
++---------+----------+------------+----------------+
+| prod_id | quantity | item_price | expanded_price |
++---------+----------+------------+----------------+
+| ANV01   |       10 |       5.99 |          59.90 |
+| ANV02   |        3 |       9.99 |          29.97 |
+| TNT2    |        5 |      10.00 |          50.00 |
+| FB      |        1 |      10.00 |          10.00 |
++---------+----------+------------+----------------+
+4 rows in set (0.00 sec)
+
+mysql> 
